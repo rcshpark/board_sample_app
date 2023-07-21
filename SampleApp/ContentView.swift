da2f8c4037404bf42c02baeded40e878fedde362
rcshpark <rcshpark@gmail.com>
2023-07-21 22:58:36 +0900

    Initial Commit

---
+++ SampleApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SampleApp
+//
+//  Created by 박상훈 on 2023/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by 박상훈 on 2023/07/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
